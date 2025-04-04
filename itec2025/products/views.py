0f0de9aad5c877751b77f61adffd478b50c42fac
matias <matiasjavierlucero@gmail.com>
2025-04-03 21:21:10 -0300

    update files

--- itec2025/products/views.py
+++ itec2025/products/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
-# Create your views here.
+from products.services.products import ProductService
+
+def product_list(request):
+    all_products = ProductService.get_all()
+
+    return render(
+        request, 
+        'products/list.html',
+        {
+            'products': all_products,
+            'otro_atributo': 'Atributo 2'
+        }
+    )
+
+def order_list(request):
+    return render(request, 'orders/list.html')
