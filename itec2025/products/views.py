5594d96157f7202cb5e3c303e255b3fcb83bb34a
matias <matiasjavierlucero@gmail.com>
2025-04-09 22:26:02 -0300

    primera parte clase 9-4-25

--- itec2025/products/views.py
+++ itec2025/products/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
+from products.models import Product
 from products.services.products import ProductService
 
 def product_list(request):
@@ -8,10 +9,20 @@ def product_list(request):
     return render(
         request, 
         'products/list.html',
-        {
-            'products': all_products,
-            'otro_atributo': 'Atributo 2'
-        }
+        dict(
+            products=all_products,
+            otro_atributo='Atributo 2'
+        )
+    )
+
+def product_detail(request, product_id):
+    product = get_object_or_404(Product, id=product_id)
+    return render(
+        request,
+        'products/detail.html',
+        dict(
+            product=product,
+        )
     )
 
 def order_list(request):
