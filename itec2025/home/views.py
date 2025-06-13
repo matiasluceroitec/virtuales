6ecb93b4832f0e2d589b18ecb0f15a0a953026fb
matias <matiasjavierlucero@gmail.com>
2025-06-12 21:06:11 -0300

    update code

--- itec2025/home/views.py
+++ itec2025/home/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
 from django.core.mail import EmailMessage
 from django.shortcuts import redirect, render
+from django.template.loader import render_to_string
 from django.views import View
 
 from home.forms import LoginForm, RegisterForm
@@ -48,18 +49,17 @@ class RegisterView(View):
             )
 
             subject = 'Bienvenido al sistema'
-            message = f'''
-                    Bienvedio {user.username} 
-                    Gracias por registrarte bajo el email
-                    {user.email}
-                    '''
+            message = render_to_string(
+                'emails/welcome.html',
+                {"email": user.email}
+            )
             email = EmailMessage(
                 subject=subject,
                 body=message,
                 from_email=settings.EMAIL_HOST_USER,
                 to=[user.email]
             )
-
+            email.content_subtype = 'html'
             email.send(
                 fail_silently=False
             )
